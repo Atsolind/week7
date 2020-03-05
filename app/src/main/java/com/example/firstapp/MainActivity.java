e91c6a5f76de11cccdca9e85fdb98a0cc6acd297
Atsolind <atso.lindstrom@student.lut.fi>
2020-03-05 16:37:12 +0200

    exercise 7.1

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //this.testFunction();
+        //text = (TextView) findViewById(R.id.textView);
+    }
+    public void testFunction(View v) {
+        System.out.println("Hello World!");
+        //text.setText("MOIKKA MAAILMA");
     }
 }
