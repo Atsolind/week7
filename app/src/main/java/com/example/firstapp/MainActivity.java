c955ad32ec598198c7981453c14d615480a9aa10
Atsolind <atso.lindstrom@student.lut.fi>
2020-03-21 14:47:39 +0200

    exercise 7.3

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -4,21 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    String input;
     TextView text;
+    EditText user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this.testFunction();
         text = (TextView) findViewById(R.id.textView);
+        user = (EditText) findViewById(R.id.userInput);
+
     }
     public void testFunction(View v) {
-        //System.out.println("Hello World!");
-        text.setText("Hello World!");
-    }
+        input = user.getText().toString();
+        text.setText(input); }
+
+
 }
