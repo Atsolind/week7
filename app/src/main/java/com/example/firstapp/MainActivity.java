86d4f9281cacd097842bf5f9b0cbb7f7347f2bb1
Atsolind <atso.lindstrom@student.lut.fi>
2020-03-22 13:03:23 +0200

    exercise 7.5

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         }
         catch (IOException e){
             Log.e("IOException", "Virhe syötteessä");
+            
         }
 
     }
